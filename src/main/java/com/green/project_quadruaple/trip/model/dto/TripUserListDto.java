fc843553db64c2daf9fe063097227e541a379aec
unknown <wjddnr1gh@naver.com>
2025-02-13 16:15:34 +0900

    여행 상세 참여유저 정보 수정

---
+++ src/main/java/com/green/project_quadruaple/trip/model/dto/TripUserListDto.java
@@ -0,0 +1,15 @@
+package com.green.project_quadruaple.trip.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class TripUserListDto {
+
+    private String email;
+    private String name;
+    private String profilePic;
+}
